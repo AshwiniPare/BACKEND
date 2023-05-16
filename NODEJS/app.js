7603b9723e8a4bf7cfc3d8ce270df83bda4373de
AshwiniPare <ashwinipbhatt@gmail.com>
2023-05-16 14:19:53 -0700

    redirecting and body parser

--- NODEJS/app.js
+++ NODEJS/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true})); 
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button>');
 });
-app.use((req, res, next) => {
-    console.log('In the another middleware');
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Hi from Express!</h1>');
-   // res.send( '{ key1: value }');
 });
 
 app.listen(3000);
