127ce51d61d971ebaa48d950d24de0a0e6aa118a
AshwiniPare <ashwinipbhatt@gmail.com>
2023-05-17 15:09:30 -0700

    html,css

--- NODEJS/routes/admin.js
+++ NODEJS/routes/admin.js
@@ -1,14 +1,26 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('inside req');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/shop/');
 });
+
+router.get('/contactus', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contactus.html'));
+});
+
+router.use('/success', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'success.html'));
+});
+
 module.exports = router;
