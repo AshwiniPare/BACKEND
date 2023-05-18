b7c67d4149751ae35113a7b6ebca64b905ccc90b
AshwiniPare <ashwinipbhatt@gmail.com>
2023-05-18 13:59:46 -0700

    MVC

--- NODEJS/controllers/products.js
+++ NODEJS/controllers/products.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const rootDir = require('../util/path');
 
-exports.getAddProduct = (req, res, next) => {
+/*exports.getAddProduct = (req, res, next) => {
     console.log('inside req');
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 };
@@ -13,4 +13,32 @@ exports.postAddProduct =  (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+};*/
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
 };
+
+exports.postAddProduct =  (req, res, next) => {
+    res.redirect('/');
+  };
+
+  exports.getProducts = (req, res, next) => {
+    const products = adminData.products;
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  };
+  
